d9f7c56c25d6106aba79c97817fa73f022dc7158
Rengga Maulana <renggaa21@gmail.com>
2021-09-03 13:23:34 +0700

    add some components

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (   
+        <>
+    <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"/>
+    </Head>
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae possimus iusto non tempora. Totam dicta temporibus labore optio voluptate maxime inventore perferendis. Autem quibusdam nulla vitae facilis natus. Nisi, dicta!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae possimus iusto non tempora. Totam dicta temporibus labore optio voluptate maxime inventore perferendis. Autem quibusdam nulla vitae facilis natus. Nisi, dicta!</p>
+        </div>
+        </>
+    );
+}
+ 
+export default About;
